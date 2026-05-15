1b0f9b1527e417473173436643bb7971c6c201a4
Siranjeevi Kannan <siranjeevikannan26@gmail.com>
2026-05-15 18:44:40 +0530

    Add files via upload
    Compared N-grams

--- Spam detection/main_tfidf.py
+++ Spam detection/main_tfidf.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import nltk
 from nltk.tokenize import word_tokenize
@@ -45,7 +45,8 @@ lemmentizer = WordNetLemmatizer()
 lemmed_msg = [[lemmentizer.lemmatize(word) for word in d] for d in filtered_msg]
 
 prevector_msg = [' '.join(d) for d in lemmed_msg]
-vectorizer = CountVectorizer(ngram_range=(2,2))
+
+vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(prevector_msg)
 features = vectorizer.get_feature_names_out()
 
@@ -64,7 +65,7 @@ ypred = cls.predict(xtest)
 total = ytest.size
 count  = sum(ytest != ypred)
 
-# print("total",total,count )
+print("total",total,count )
 #total 1115 35
 
 true_positive = sum((ypred==1)&(ytest==1))
@@ -83,68 +84,53 @@ print("Precision ",precision)
 print("Recall ",recall)
 print("F1 ",f1)
 
-#ngram_range(1,1)
-# True Positives  137
-# False positives  22
-# Flase negatives  13
-# Precision  0.8616352201257862
-# Recall  0.9133333333333333
-# F1  0.8867313915857605
+
+#----------Before Custom Preprocessing(Tf-idf)----------
+
+# True Positives  108
+# False positives  0
+# Flase negatives  42
+# Precision  1.0
+# Recall  0.72
+# F1  0.8372093023255813
 
 #ngram_range(1,2)
-# True Positives  141
-# False positives  29
-# Flase negatives  9
-# Precision  0.8294117647058824
-# Recall  0.94
-# F1  0.88125
-
+# True Positives  90
+# False positives  0
+# Flase negatives  60
+# Precision  1.0
+# Recall  0.6
+# F1  0.7499999999999999
 
 #ngram_range(2,2)
-# True Positives  145
-# False positives  225
-# Flase negatives  5
-# Precision  0.3918918918918919
-# Recall  0.9666666666666667
-# F1  0.5576923076923077
+# True Positives  85
+# False positives  0
+# Flase negatives  65
+# Precision  1.0
+# Recall  0.5666666666666667
+# F1  0.7234042553191489
 
-'''
-False Positive
-tell reached
-k call ah
-okay name ur price long legal wen pick u ave x am xx
-dont worry guess busy
-new car house parent new job hand
-evo download flash jealous
-need coffee run tomo believe time week already
-lol yes friendship hanging thread cause u buy stuff
-coffee cake guess
-ì_ come
-know anthony bringing money school fee pay rent stuff like thats need help friend need
-get ur st ringtone free reply msg tone gr top tone phone every week å per wk opt send stop
-sorry call later
-quite late lar ard anyway wun b drivin
-hmmm thought said hour slave late punish
-time fix spelling sometimes get completely diff word go figure
-meanwhile shit suite xavier decided give u lt gt second warning samantha coming playing jay guitar impress shit also think doug realizes live anymore
-love come took long leave zaher got word ym happy see sad left miss
-shall fine avalarr hollalater
-aight ill get fb couple minute
-'''
+#----------After Custom Preprocessing----------
 
-'''
-False Negative
-k tell anything
-would really appreciate call need someone talk
-always putting business put picture as facebook one open people ever met would think picture room would hurt make feel violated
-gon na go get taco
-wait know wesley town bet hella drug
-lol know dramatic school already closed tomorrow apparently drive inch snow supposed get
-opinion jada kusruthi lovable silent spl character matured stylish simple pls reply
-urgent call landline complimentary ibiza holiday cash await collection sae c po box sk wp ppm
-nutter cutter ctter cttergg cttargg ctargg ctagg ie
-hey gave photo registered driving ah tmr wan na meet yck
-ur cash balance currently pound maximize ur cash send go p msg cc po box tcr w
-yup bathe liao
-free call sir waiting
-'''
+# True Positives  106
+# False positives  0
+# Flase negatives  44
+# Precision  1.0
+# Recall  0.7066666666666667
+# F1  0.828125
+
+#ngram_range(1,2)
+# True Positives  95
+# False positives  0
+# Flase negatives  55
+# Precision  1.0
+# Recall  0.6333333333333333
+# F1  0.7755102040816326
+
+#ngram_range(2,2)
+# True Positives  78
+# False positives  0
+# Flase negatives  72
+# Precision  1.0
+# Recall  0.52
+# F1  0.6842105263157895
